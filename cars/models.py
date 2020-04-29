828fab5482e2ddff9c4d4e9a795f84e177e5cbf0
Alberto <ajpernaletel@gmail.com>
2020-04-29 18:07:47 -0400

    Se renderizo el index, y se creo una app para Cars y otra para Clientes.

---
+++ cars/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Cars(models.Model):
+    """marca_car =
+    modelo_car =
+    color_car =
+    tipo_car =
+    patente =
+    num_puertas =
+    tipo_consumo =
+    tamaño =
+    cliente_car =
+    created_at ="""
+    pass
+    
