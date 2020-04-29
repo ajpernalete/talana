d733e4eebf057f3f41f824e231412e20a62c7db9
Alberto <ajpernaletel@gmail.com>
2020-04-29 16:55:18 -0400

    Conexión y migración a MySQL Database

--- TalanaRentCars/settings.py
+++ TalanaRentCars/settings.py
@@ -72,12 +72,23 @@ WSGI_APPLICATION = 'TalanaRentCars.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'talana_rent_cars',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
+    } 
 }
 
 
